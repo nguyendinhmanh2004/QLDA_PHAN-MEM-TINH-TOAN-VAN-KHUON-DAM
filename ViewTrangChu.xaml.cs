6e7b599e9547722c87baa1b4a1c14e3fa90dc713
nguyendinhmanh2004 <0141967@st.huce.edu.vn>
2025-05-31 21:46:14 +0700

    Add files via upload

---
+++ ViewTrangChu.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using TINHTOANVANKHUONDAM.Class;
+using TINHTOANVANKHUONDAM.ViewModels;
+
+namespace TINHTOANVANKHUONDAM.View
+{
+    /// <summary>
+    /// Interaction logic for ViewTrangChu.xaml
+    /// </summary>
+    public partial class ViewTrangChu : Window
+    {
+        public bool dialogResult = false;
+        public Document Doc;
+
+        public ViewTrangChu(Document document,UIDocument uiDoc)
+        {
+            InitializeComponent();
+            Doc = document;
+            dialogResult = true;
+            this.DataContext = new BeamVM(document,uiDoc);
+            dtgBeam.ItemsSource = clsBienToanCuc.Beams;
+        }
+    }
+
+}
