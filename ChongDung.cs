b1d3b107d159b5ccd021e58ff1c04130282c5d31
btdat0214867 <0214867@st.huce.edu.vn>
2025-05-31 21:41:07 +0700

    Add files via upload

---
+++ ChongDung.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TINHTOANVANKHUONDAM.Class
+{
+    public class ChongDung
+    {
+        private double lucDocTruyen_kN;
+        private double tietDien_mm2;
+        private double ungSuatChoPhep_MPa;
+
+        public double LucDocTruyen_kN
+        {
+            get { return lucDocTruyen_kN; }
+            set { lucDocTruyen_kN = value; }
+        }
+
+        public double TietDien_mm2
+        {
+            get { return tietDien_mm2; }
+            set { tietDien_mm2 = value; }
+        }
+
+        public double UngSuatChoPhep_MPa
+        {
+            get { return ungSuatChoPhep_MPa; }
+            set { ungSuatChoPhep_MPa = value; }
+        }
+
+        public double TinhUngSuatThucTe_MPa()
+        {
+            return (lucDocTruyen_kN * 1000) / tietDien_mm2;
+        }
+
+        public bool KiemTraChongDung()
+        {
+            return TinhUngSuatThucTe_MPa() <= ungSuatChoPhep_MPa;
+        }
+    }
+}
